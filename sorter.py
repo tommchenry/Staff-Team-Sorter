2274bbf7c676a1b3fd84e738a3ba073f0f299f05
Tom McHenry <tommchenry@gmail.com>
2014-10-24 13:08:05 -0500

    Initial file creation.
    Created script.

---
+++ sorter.py
@@ -0,0 +1,9 @@
+def sortTeams(allStaff):
+	'''
+	Takes a list of all staff members and returns a list of staff
+	sorted into randomized two-person teams.
+	'''
+	return allStaff
+
+allStaff=['The Guy','The Girl','Paisley','Boo Radley']
+sortTeams(allStaff)
