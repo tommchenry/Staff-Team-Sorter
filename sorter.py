325f8a03b9dca744547084a2505cc7bead914add
Tom McHenry <tommchenry@gmail.com>
2014-10-24 13:55:15 -0500

    Added some initial code to deal with #1
    The code will now shuffle a list into a random order.

--- sorter.py
+++ sorter.py
@@ -1,9 +1,16 @@
+import random
 def sortTeams(allStaff):
 	'''
 	Takes a list of all staff members and returns a list of staff
 	sorted into randomized two-person teams.
 	'''
-	return allStaff
+	newTeams=[]
+	staffCopy=allStaff
+	for i in range(len(allStaff)):
+	    selectedStaff=random.randrange(0, len(staffCopy))
+	    newTeams.append(staffCopy[selectedStaff])
+	    staffCopy.remove(staffCopy[selectedStaff])
+	print newTeams 	
 
 allStaff=['The Guy','The Girl','Paisley','Boo Radley']
 sortTeams(allStaff)
